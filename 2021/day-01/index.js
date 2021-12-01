778ae02e5ed335f6bebc12ace80a50fda923c5e0
David Wesst <david@cocobokostudios.com>
2021-12-01 12:04:23 -0600

    day-01 parsed input into array

--- 2021/day-01/index.js
+++ 2021/day-01/index.js
@@ -1,1 +1,15 @@
-console.log('hello AoC!');
+import { readFile } from 'fs/promises';
+
+try {
+    // extract the depth data
+    const filePromise = readFile(new URL(`./input.txt`, import.meta.url), { encoding: 'utf-8'});
+    const data = await filePromise;
+    const depths = data.split('\n');
+    console.log(depths);
+
+    // analyze the data
+
+    // display the results
+} catch (err) {
+    console.error(err);
+}
