c71743dd3d8eec426017a167684b81322d32e1e3
David Wesst <david@cocobokostudios.com>
2021-12-02 22:15:46 -0600

    completed day 02, Dive!

--- 2021/node/day-02/index.js
+++ 2021/node/day-02/index.js
@@ -6,6 +6,9 @@ import { printBanner } from "../utils/print.js";
  * @param {Boolean} showWork Print out verbose work statements as it goes.
  */
 const part1 = async function (showWork = false) {
+    // output banner
+    printBanner('Day 2, Part I');
+
     // read input
     const fileURL = new URL(`./input.txt`, import.meta.url);
     const rawCommandData = await readFileByLine(fileURL);
@@ -58,13 +61,67 @@ const part1 = async function (showWork = false) {
 }
 
 /**
- * Day 2, Part I
+ * Day 2, Part II
  * @param {Boolean} showWork Print out verbose work statements as it goes.
  */
-const part2 = (showWork = false) => {
-    console.log("part 2");
+const part2 = async function(showWork = false) {
+    // output banner
+    printBanner('Day 2, Part II');
+
+    // read input
+    const fileURL = new URL(`./input.txt`, import.meta.url);
+    const rawCommandData = await readFileByLine(fileURL);
+
+    // parse the commands
+    let commands = [];
+    for (let index = 0; index < rawCommandData.length; index++) {
+        const element = rawCommandData[index].split(' ');
+
+        const command = { 
+            instruction: element[0],
+            delta: parseInt(element[1])
+        };
+        commands.push(command);
+
+        if(showWork) {
+            console.log(command);
+        }
+    }
+
+    // calculate depth and horizontal movement
+    let aim = 0;
+    let depth = 0;
+    let horizontal = 0;
+
+    commands.forEach(c => {
+        switch (c.instruction) {
+            case 'forward':
+                horizontal += c.delta;
+                depth += aim * c.delta;
+                break;
+            case 'up':
+                aim -= c.delta;
+                break;
+            case 'down':
+                aim += c.delta;
+                break;
+            default:
+                console.error(`Not built to handle ${c.instruction}`);
+                break;
+        }
+
+        if(showWork) {
+            console.log(`(${c.instruction} ${c.delta}) : H = ${horizontal} D = ${depth} A= ${aim}`);
+        }
+    });
+
+    // output the values
+    console.log(`Horizontal: ${horizontal}`);
+    console.log(`Depth:    : ${depth}`);
+    console.log(`Aim:      : ${aim}`);
+    console.log(`Answer    : ${horizontal * depth}`);
 }
 
 // run
-part1(true);
-part2();
+await part1();
+await part2();
