d56f26d8b80d325da61bfdd2393fecc075229cc5
David Wesst <867084+davidwesst@users.noreply.github.com>
2021-12-03 05:11:45 +0000

    day 03 scaffold

---
+++ 2021/node/day-03/index.js
@@ -0,0 +1,10 @@
+const part1 = async function(showWork = false) {
+    console.log("part 1");
+}
+
+const part2 = async function(showWork = false) {
+    console.log("part 2");
+}
+
+await part1();
+await part2();
