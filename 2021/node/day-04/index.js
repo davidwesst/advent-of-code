9c32324ee145acead2a90a9487158fded8c67222
David Wesst <david@cocobokostudios.com>
2021-12-04 20:39:25 -0600

    setup scaffold for day 04 and added devcontainer

---
+++ 2021/node/day-04/index.js
@@ -0,0 +1,12 @@
+import { printBanner } from '../utils/print.js';
+
+const part1 = async function(showWork = false) {
+    printBanner('Day 04, Part I');
+}
+
+const part2 = async function(showWork = false) {
+    printBanner('Day 04, Part II');
+}
+
+await part1();
+await part2();
